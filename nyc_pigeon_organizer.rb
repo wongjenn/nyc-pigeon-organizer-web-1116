4b6e7e00e2f4ab6614fa090b0658b6f605bf1554
Jennifer Wong <wongjenn@yahoo.com>
2016-11-14 18:33:49 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,14 @@
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  result = {}
+  data.each do |attributes, data_items|
+    data_items.each do |item, names|
+      names.each do |name|
+        result[name] ||= {}
+        result[name][attributes] ||= []
+        result[name][attributes] << item.to_s
+      end
+    end
+  end
+
+  result
 end
